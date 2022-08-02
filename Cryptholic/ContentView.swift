c23712abc4144cdf0d31c1e9d29b47174873b3a9
Berkay-Disli <berkaydisli.work@gmail.com>
2022-08-02 13:01:52 +0300

    Initial Commit

---
+++ Cryptholic/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Cryptholic
+//
+//  Created by Berkay Disli on 2.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cryptholic/CryptholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptholicApp.swift
+//  Cryptholic
+//
+//  Created by Berkay Disli on 2.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
